0a10c4a295550b9912f1e512882b9c7c7346e80c
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2019-11-24 13:04:39 -0500

    added auth context provider

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends React.Component {
+    // This takes any data attached to the value variable in the ThemeContext component\
+    // We can then use that components state in our app below
+    static contextType = ThemeContext;
+
     render () {
+        // this.context replaces this.props
+        // console.log(this.context)
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className="book-list">
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
                 <ul>
-                    <li>The Name of The Wind</li>
-                    <li>The Great Gatsby</li>
-                    <li>The Dark Knight</li>
+                    <li style={{background: theme.ui }}>The Name of The Wind</li>
+                    <li style={{background: theme.ui }}>The Great Gatsby</li>
+                    <li style={{background: theme.ui }}>The Dark Knight</li>
                 </ul>
             </div>
         )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends React.Component {
-
-    // This takes any data attached to the value variable in the ThemeContext component\
-    // We can then use that components state in our app below
-    static contextType = ThemeContext;   
-
+    // using the Consumer we can do the same thing as before but import the context
+    // inside of the return and deal with state internally
+    // we can use the Consumer in functional components and use multiple contexts inside one component
+    
     render() {
-        // this.context replaces this.props
-        // console.log(this.context)
-
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
         return (
-            <nav style={{ background: theme.ui, color: theme.syntax}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax}}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                )
+            }}</ThemeContext.Consumer>
         )
     }
 };
